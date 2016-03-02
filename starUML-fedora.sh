d6716c1cc6073b8f0610a0753a9a2f153467e5ee
Pedro Rodríguez de Oliveira <WolfangAukang@users.noreply.github.com>
2016-03-02 13:41:44 -0600

    Update starUML-fedora.sh
    Remotion of sudo su

--- starUML-fedora.sh
+++ starUML-fedora.sh
@@ -4,6 +4,12 @@
 # Made by Pedro R. de Oliveira (WolfangAukang)
 #NOTE: Please, install binutils before executing this script. I've checked Fedora Spin KDE 22 and it doesn't have this installed as default.
 
+
+if [[ ! $(whoami) = "root" ]]; then
+    echo "Please, execute this as root. It is required to do certain operations"
+    exit 1
+fi
+
 echo "Downloading package..."
 wget http://staruml.io/download/release/v2.6.0/StarUML-v2.6.0-64-bit.deb
 
@@ -11,8 +17,7 @@ echo "Extracting files..."
 ar vx StarUML-v2.6.0-64-bit.deb
 tar -xf data.tar.gz
 
-echo "Installing and linking additonal dependencies... (Please, insert your pass to become superuser)"
-sudo su
+echo "Installing and linking additonal dependencies..."
 dnf -y install systemd-libs
 ln -s /usr/lib64/libudev.so.1 /usr/lib64/libudev.so.0
 
